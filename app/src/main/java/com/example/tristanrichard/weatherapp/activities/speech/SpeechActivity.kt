a2dedc364230b485cabfa41c47883e69c806a381
Tristan Richard <tristangrichard@icloud.com>
2018-04-14 10:06:55 +0200

    Activities: Created speech and weather activities

---
+++ app/src/main/java/com/example/tristanrichard/weatherapp/activities/speech/SpeechActivity.kt
@@ -0,0 +1,13 @@
+package com.example.tristanrichard.weatherapp.activities.speech
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tristanrichard.weatherapp.R
+
+class SpeechActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_speech)
+    }
+}
---
+++ app/src/main/java/com/example/tristanrichard/weatherapp/activities/weather/WeatherActivity.kt
@@ -0,0 +1,13 @@
+package com.example.tristanrichard.weatherapp.activities.weather
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tristanrichard.weatherapp.R
+
+class WeatherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather)
+    }
+}
